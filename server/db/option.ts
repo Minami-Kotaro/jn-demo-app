e90caede9094d99a446a229eb162647fd6f8f6d5
minami kotaro <minami-kotaro@ksc.co.jp>
2022-01-03 10:12:14 +0900

    修正

--- server/db/option.ts
+++ server/db/option.ts
@@ -12,7 +12,7 @@ const sslOption = isProduction
       ssl: true,
       extra: {
         ssl: {
-          rejectunauthorized: false,
+          rejectUnauthorized: false,
         },
       },
     }
